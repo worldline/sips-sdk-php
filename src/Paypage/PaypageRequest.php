18bede026f397815a20fd82f38bcbd875ed68bf5
Guislain Duthieuw <gduthieuw@houra.fr>
2019-01-14 18:13:22 +0100

    Upgrade interface version

--- src/Paypage/PaypageRequest.php
+++ src/Paypage/PaypageRequest.php
@@ -120,7 +120,7 @@ class PaypageRequest extends SipsMessage
     public function __construct()
     {
         $this->serviceUrl = "rs-services/v2/paymentInit";
-        $this->interfaceVersion = "IR_WS_2.19";
+        $this->interfaceVersion = "IR_WS_2.23";
         $this->setTransactionReference($this->generateReference());
     }
 
