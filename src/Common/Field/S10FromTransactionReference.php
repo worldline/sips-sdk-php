290933d65844c660f33b2d7b1b2d2f58b851365e
Guislain Duthieuw <gduthieuw@houra.fr>
2019-04-05 11:34:58 +0200

    New Field type : S10FromTransactionReference

---
+++ src/Common/Field/S10FromTransactionReference.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Worldline\Sips\Common\Field;
+
+class S10FromTransactionReference extends \Worldline\Sips\Common\Field
+{
+    protected $s10FromTransactionId;
+
+    protected $s10FromTransactionIdDate;
+
+    public function getS10TransactionId()
+    {
+        return $this->s10FromTransactionId;
+    }
+
+    public function getS10TransactionIdDate()
+    {
+        return $this->s10FromTransactionIdDate;
+    }
+
+    public function setS10TransactionId($s10TransactionId)
+    {
+        $this->s10FromTransactionId = $s10TransactionId;
+        return $this;
+    }
+
+    public function setS10TransactionIdDate($s10TransactionIdDate)
+    {
+        $this->s10FromTransactionIdDate = $s10TransactionIdDate;
+        return $this;
+    }
+
+
+}
