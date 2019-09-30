5538fe3855d26e42e17c1e1f8c4154c56aa31904
Guislain Duthieuw <gduthieuw@houra.fr>
2019-09-30 15:11:31 +0200

    Add DeletePaymentMean from wallet service

---
+++ src/Wallet/DeletePaymentMeanRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Worldline\Sips\Wallet;
+
+/**
+ * This function allows you to update one of the payment means contained in the client's wallet.
+ * If the account or the payment mean do not exist, a 01 response code is returned.
+ * If the update works, 00 response code is returned and the date of update.
+ * 
+ * @author Guiled <guislain.duthieuw@gmail.com>
+ */
+class DeletePaymentMeanRequest extends \Worldline\Sips\SipsRequest
+{
+    protected $merchantWalletId;
+    protected $paymentMeanId;
+    protected $intermediateServiceProviderId;
+
+    /**
+     * @author Guiled <guislain.duthieuw@gmail.com>
+     */
+    public function __construct()
+    {
+        $this->connecter        = \Worldline\Sips\Common\SipsEnvironment::OFFICE;
+        $this->serviceUrl       = "rs-services/v2/wallet/deletePaymentMean";
+        $this->interfaceVersion = "WR_WS_2.12";
+    }
+
+    public function getMerchantWalletId()
+    {
+        return $this->merchantWalletId;
+    }
+
+    public function getPaymentMeanId()
+    {
+        return $this->paymentMeanId;
+    }
+
+    public function getIntermediateServiceProviderId()
+    {
+        return $this->intermediateServiceProviderId;
+    }
+
+    public function setMerchantWalletId($merchantWalletId)
+    {
+        $this->merchantWalletId = $merchantWalletId;
+        return $this;
+    }
+
+    public function setPaymentMeanId($paymentMeanId)
+    {
+        $this->paymentMeanId = $paymentMeanId;
+        return $this;
+    }
+
+    public function setIntermediateServiceProviderId($intermediateServiceProviderId)
+    {
+        $this->intermediateServiceProviderId = $intermediateServiceProviderId;
+        return $this;
+    }
+
+
+}
