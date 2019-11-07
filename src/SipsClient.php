efec7eae043283e9fc01903e38bd97d7c6ad9fdc
Nicolas Ros <Nicolas@MBP.local>
2019-11-07 11:01:27 +0100

    Correct creation of InitializationResponse.

--- src/SipsClient.php
+++ src/SipsClient.php
@@ -91,7 +91,7 @@ class SipsClient
         $request = new Request("POST", $paymentRequest->getServiceUrl(), $headers, $json);
         $response = $client->send($request);
         $this->lastResponseAsJson = $response->getBody()->getContents();
-        $initialisationResponse = new InitializationResponse($this->lastResponseAsJson, true));
+        $initialisationResponse = new InitializationResponse(json_decode($this->lastResponseAsJson, true));
         if (!is_null($initialisationResponse->getSeal())) {
             $validSeal = $sealCalculator->isCorrectSeal($initialisationResponse, $this->getSecretKey());
             if (!$validSeal) {
