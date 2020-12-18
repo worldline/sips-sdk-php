b122a412b73fd302042ff643b86880656c3974f6
Guislain Duthieuw <gduthieuw@houra.fr>
2020-12-18 11:05:35 +0100

    New file to contain all constant values

---
+++ src/Common/Values.php
@@ -0,0 +1,7 @@
+<?php
+namespace Worldline\Sips\Values\paymentMeanBrandSelectionStatus {
+    const DEFAULT_MODE = 'APPLIED_DEFAULT';
+    const HOLDER = 'APPLIED_HOLDER';
+    const NOT_APPLICABLE = 'NOT_APPLICABLE';
+}
+
